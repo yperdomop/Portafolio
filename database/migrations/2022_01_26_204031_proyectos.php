7a6808a3e2b2277df3fe3eb57b6100a8dad74cf6
= <=>
2022-01-30 15:18:26 -0500

    commit

---
+++ database/migrations/2022_01_26_204031_proyectos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Proyectos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->string('nombre');
+            $table->string('imagen');
+            $table->string('descripcion');
+            $table->string('url');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
